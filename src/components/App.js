186f7e55e497e918db286fbae99f6f34f263c88b
Kade Dancy <kadedancy@gmail.com>
2025-07-31 14:04:08 -0400

    Flushed out components and added to about page.

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello, I'm Kade Dancy</h1>
-      <p>Welcome to my portfolio website!</p>
+      <div className="MainDiv">      
+        <p>Hello, I'm Kade Dancy</p>
+        <p>I am a Senior pursuing a B.S. in Computer Engineering Technology with a minor in Mathematical Sciences.
+          I expect to graduate in May 2026. I enjoy building creative software and hardware solutions for embedded systems.
+          I am committed to pursuing a career in Software Engineering</p>
+        <p>Through my academic and personal pursuits, I've been able to learn and develop proficiency in multiple programming languages,
+           development environments, version control, and analytical problem solving. </p>
+
+        <h1 className="SectionLine">Skills</h1>
+        <p className="SectionItems">Arduino Python C/C++ HTML CSS SQL JavaScript Git Github</p> 
+        <p className="SectionItems">VSCode VisualStudio STM32 KiCAD Multisim MicrosoftSuite</p>
+      </div>
     </div>
   );
 }
